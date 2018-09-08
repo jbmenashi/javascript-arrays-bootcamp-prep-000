c59effedc3792e48ee5d84334baf1fa5af701412
Jacob Menashi <jbmenashi@gmail.com>
2018-09-08 14:00:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -46,3 +46,9 @@ function removeElementFromEndOfArray(array) {
   var array = array.slice(0, array.length - 1);
   return array;
 }
+
+
+var items = [1, 2, 3, 4, 5];
+console.log([...items.slice(0,2), ...items.slice(3)]);
+
+
